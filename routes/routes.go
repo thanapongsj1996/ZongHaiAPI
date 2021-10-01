bbba065d3ac142cd18d706655e8bf06879c1ca1c
thanapongsj <thanapongsj1996@gmail.com>
2021-10-02 01:36:26 +0700

    Added health check endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,10 @@ func Serve(r *gin.Engine) {
 	db := config.GetDB()
 	v1 := r.Group("/api/v1")
 
+	v1.GET("/health-check", func(ctx *gin.Context) {
+		ctx.Status(200)
+	})
+
 	customerController := controllers.Customer{DB: db}
 	jobsGroup := v1.Group("jobs")
 	{
